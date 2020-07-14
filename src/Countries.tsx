deb0a094b6ce479870ec967e6998599f66ec25ea
Ora Martindale <ora@martindale.org>
2020-07-14 11:39:18 -0500

    Skills Assessment: Add Ability to Filter Countries

--- src/Countries.tsx
+++ src/Countries.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 
 interface Country {
   name: string;
@@ -13,6 +13,7 @@ interface Country {
 function Countries() {
   const [countries, setCountries] = useState<Array<Country>>([]);
   const [sortDirection, setSortDirection] = useState<string>('desc');
+  const [filterValue, setFilterValue] = useState<string>('');
 
   const sortDesc = (firstElement: Country, secondElement: Country) =>
     firstElement.population > secondElement.population ? 1 : -1;
@@ -24,14 +25,41 @@ function Countries() {
     setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
     setCountries(countries.sort(sortDirection === 'asc' ? sortAsc : sortDesc));
   };
-
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all?fields=name;capital;currencies;languages;alpha3code;population;flag')
+  const fetchAllCountries = () => {
+    setSortDirection('desc');
+    fetch('https://restcountries.eu/rest/v2/all?fields=name;capital;currencies;languages;alpha3Code;population;flag')
       .then(response => response.json())
       .then(c => c.sort(sortAsc))
       .then(setCountries)
       .catch(console.error);
+  };
+
+  const filterCountries = function(event: ChangeEvent<HTMLInputElement>) {
+    setFilterValue(event.target.value);
+  };
+
+  useEffect(() => {
+    fetchAllCountries();
   }, []);
+  useEffect(() => {
+    if (filterValue === '') {
+      fetchAllCountries();
+    } else {
+      let lowerCaseFilterValue = filterValue.toLowerCase();
+      let filteredCountries: Array<Country> = [];
+      for (var i = 0, l = countries.length, country; i < l; i++) {
+        country = countries[i];
+        if (
+          country.name.toLowerCase().includes(lowerCaseFilterValue) ||
+          country.alpha3Code.toLowerCase().includes(lowerCaseFilterValue)
+        ) {
+          filteredCountries.push(country);
+        }
+      }
+      console.log(filteredCountries);
+      setCountries(filteredCountries);
+    }
+  }, [filterValue]);
 
   return (
     <table>
@@ -44,6 +72,11 @@ function Countries() {
         </tr>
       </thead>
       <tbody>
+        <tr>
+          <td colSpan={2}>
+            <input placeholder="Filter By Country or Code" value={filterValue} onChange={filterCountries} />
+          </td>
+        </tr>
         {countries.map((row, item) => (
           <tr key={item}>
             <td>
